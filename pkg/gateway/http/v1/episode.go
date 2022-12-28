0a8317cd2fc75902c00376fcd913719a5763bf8a
RikiyaFujii <ridai22hanriku@gmail.com>
2022-12-29 00:33:30 +0900

    feat: v1

--- pkg/gateway/http/v1/episode.go
+++ pkg/gateway/http/v1/episode.go
@@ -5,13 +5,28 @@ import (
 
 	"github.com/go-chi/chi"
 
+	"github.com/CyberAgentHack/server-performance-tuning-2023/pkg/gateway/http/request"
 	"github.com/CyberAgentHack/server-performance-tuning-2023/pkg/gateway/http/response"
+	"github.com/CyberAgentHack/server-performance-tuning-2023/pkg/usecase"
 )
 
 func (s *Service) routeEpisode(r chi.Router) {
-	r.Get("/", s.listEpisode)
+	r.Get("/", s.listEpisodes)
 }
 
-func (s *Service) listEpisode(w http.ResponseWriter, r *http.Request) {
-	response.OK(nil, w, r)
+func (s *Service) listEpisodes(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	req := &usecase.ListEpisodesRequest{
+		PageSize: request.QueryIntDefault(r, "pageSize", 20),
+		SeriesID: request.Query(r, "seriesId"),
+		SeasonID: request.Query(r, "seasonId"),
+	}
+	resp, err := s.usecase.ListEpisodes(ctx, req)
+	if err != nil {
+		response.Error(err, w, r)
+		return
+	}
+
+	response.OK(resp.Episodes, w, r)
 }
--- pkg/gateway/http/v1/series.go
+++ pkg/gateway/http/v1/series.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/go-chi/chi"
 
+	"github.com/CyberAgentHack/server-performance-tuning-2023/pkg/gateway/http/request"
 	"github.com/CyberAgentHack/server-performance-tuning-2023/pkg/gateway/http/response"
+	"github.com/CyberAgentHack/server-performance-tuning-2023/pkg/usecase"
 )
 
 func (s *Service) routeSeries(r chi.Router) {
@@ -13,5 +15,16 @@ func (s *Service) routeSeries(r chi.Router) {
 }
 
 func (s *Service) listSeries(w http.ResponseWriter, r *http.Request) {
-	response.OK(nil, w, r)
+	ctx := r.Context()
+
+	req := &usecase.ListSeriesRequest{
+		PageSize: request.QueryIntDefault(r, "pageSize", 20),
+	}
+	resp, err := s.usecase.ListSeries(ctx, req)
+	if err != nil {
+		response.Error(err, w, r)
+		return
+	}
+
+	response.OK(resp.Series, w, r)
 }
