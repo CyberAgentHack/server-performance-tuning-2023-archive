eec05c4bfe818f62e542a766a37ec9553fe25bb1
MasatoraAtarashi <atarashi.masatora@gmail.com>
2023-01-05 02:07:00 +0900

    autoScalingConfigurationを手動作成したものに変更

--- deployments/lib/performance-tuning-workshop.ts
+++ deployments/lib/performance-tuning-workshop.ts
@@ -1,12 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import {CfnOutput} from 'aws-cdk-lib';
 import * as apprunner from 'aws-cdk-lib/aws-apprunner';
-import * as cr from 'aws-cdk-lib/custom-resources';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import {Port, SecurityGroup, SubnetType} from "aws-cdk-lib/aws-ec2";
 import {Construct} from 'constructs';
-import {CreateAutoScalingConfigurationCommandInput} from '@aws-sdk/client-apprunner';
 
 export class PerformanceTuningWorkshop extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -50,31 +48,6 @@ export class PerformanceTuningWorkshop extends cdk.Stack {
         ///////////////////
         //// AppRunner ////
         ///////////////////
-        // TODO: 削除時にautoScalingConfigurationが残ってしまうので、カスタムリソースのLambdaで削除するか、CLIで削除する
-        // NOTE: 学生にオートスケーリングを設定してもらうため、デフォルトでオートスケーリングを無効にする
-        const autoScalingConfiguration: CreateAutoScalingConfigurationCommandInput = {
-            AutoScalingConfigurationName: 'auto-scaling-cfg-no-scale',
-            MaxConcurrency: 100,
-            MinSize: 1,
-            MaxSize: 1,
-        }
-
-        const createAutoScalingConfiguration = new cr.AwsCustomResource(this, 'AutoScalingConfiguration', {
-            onCreate: {
-                service: 'AppRunner',
-                action: 'createAutoScalingConfiguration',
-                parameters: autoScalingConfiguration,
-                physicalResourceId: cr.PhysicalResourceId.fromResponse('AutoScalingConfiguration.AutoScalingConfigurationArn')
-            },
-            policy: cr.AwsCustomResourcePolicy.fromSdkCalls({
-                resources: cr.AwsCustomResourcePolicy.ANY_RESOURCE
-            })
-        })
-
-        const autoScalingCfgArn = createAutoScalingConfiguration.getResponseField(
-            'AutoScalingConfiguration.AutoScalingConfigurationArn'
-        )
-
         new apprunner.CfnService(this, 'AppRunner', {
             serviceName: "wsperf-app-runner",
             sourceConfiguration: {
@@ -99,7 +72,7 @@ export class PerformanceTuningWorkshop extends cdk.Stack {
                     vpcConnectorArn: this.node.tryGetContext('vpcConnectorArn')
                 }
             },
-            autoScalingConfigurationArn: autoScalingCfgArn,
+            autoScalingConfigurationArn: this.node.tryGetContext('autoScalingConfigurationArn'),
             tags: [{
                 key: 'Project',
                 value: 'wsperf'
